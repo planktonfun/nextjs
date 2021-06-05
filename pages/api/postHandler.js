a58c677b87b9b0e21251312cdc1c56d29f3b3ad8
Paulo Castro <paulo.castro@honcho.com>
2021-06-06 03:42:17 +0800

    added a post handler

---
+++ pages/api/postHandler.js
@@ -0,0 +1,35 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  if (req.method === 'POST') {
+    // Process a POST request
+    res.json({ type: 1 });
+  }
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler
